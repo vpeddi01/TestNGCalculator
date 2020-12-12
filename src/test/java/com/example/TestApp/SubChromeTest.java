5aad5ac15f823337f50f621a52cc15c640685d06
pbeniwal <pbeniwal@yahoo.com>
2020-12-12 22:51:01 +0530

    Update SubChromeTest.java

--- src/test/java/com/example/TestApp/SubChromeTest.java
+++ src/test/java/com/example/TestApp/SubChromeTest.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 
 public class SubChromeTest {
 
-	@Test
+    @Test
     void testSub() throws IOException, InterruptedException
     {
 		WebDriver driver;
@@ -34,38 +34,37 @@ public class SubChromeTest {
 	    String myURL = "http://" + myIP + ":" + myPort + "/" + myAppName;
 	    System.out.println("Opening " + myURL);
 	    
-		System.setProperty("webdriver.chrome.driver",prop.getProperty("webdriver_path") + "chromedriver");
-		
-		System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
+	    System.setProperty("webdriver.chrome.driver",prop.getProperty("webdriver_path") + "chromedriver");
+	    System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
 	    System.setProperty("webdriver.chrome.verboseLogging", "true");
-		System.out.println("System.setProperty!!");
-		ChromeOptions chromeOptions = new ChromeOptions(); 
+	    System.out.println("System.setProperty!!");
+	    ChromeOptions chromeOptions = new ChromeOptions(); 
 	    if(System.getenv("MY_IP")!=null)
 		chromeOptions.addArguments("headless"); 
 
-		chromeOptions.addArguments("--no-sandbox");
+	    chromeOptions.addArguments("--no-sandbox");
 	    driver = new ChromeDriver(chromeOptions); 
-		System.out.println("driver!!");
+	    System.out.println("driver!!");
 
-        driver.get(myURL);
-        System.out.println("baseUrl!!");
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            driver.get(myURL);
+            System.out.println("baseUrl!!");
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         
 	    driver.findElement(By.id("f1")).sendKeys("38");
 	    driver.findElement(By.xpath("/html/body/form/input[2]")).sendKeys("18");
-        driver.findElement(By.xpath("//input[@name='r2']")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.id("s1")).click();
+            driver.findElement(By.xpath("//input[@name='r2']")).click();
+            Thread.sleep(5000);
+            driver.findElement(By.id("s1")).click();
         
-        String bodyText = driver.findElement(By.xpath("/html/body")).getText();
+            String bodyText = driver.findElement(By.xpath("/html/body")).getText();
         
-        Assert.assertTrue(bodyText.contains("20"), "20 not found!" );
+            Assert.assertTrue(bodyText.contains("20"), "20 not found!" );
         
-        Thread.sleep(5000);
+            Thread.sleep(5000);
         
-        System.out.println("Test Succeeded!!");
+            System.out.println("Test Succeeded!!");
              
-        driver.quit();  
+            driver.quit();  
 		
     }
 }
